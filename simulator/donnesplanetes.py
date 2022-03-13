da4c41290c5dfb37e9151d14a50e519e9ff3e028
anatoleNSI <91608984+anatoleNSI@users.noreply.github.com>
2022-03-13 18:51:35 +0100

    mise à jour interface
    création de zones pour accueillir les images et informations + barre d'outils

---
+++ simulator/donnesplanetes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+recherches pour les patrons
+"""
+# structure // {nom planete : {poids, rayon, distance au soleil, temps rotation soleil}"
+
+les_planètes = {'merecure' : {'3,33 x 10^23 kg', '4200 km', '46 à 70 millions de km', '87,969 j'},
+                'venus' : {'4,867 5x1024 kg', '6050 km', '104 millions de km', '243 j'},
+                'terre' : {'5,973 x 10^24', '6378 km', '150 millions de km', '365 j'},
+                'mars' : {'6,418 x 10^23','3 396 km', '227 millions de km', '696 j'},
+                'jupyter' : {'1,89 X 10^17 kg', '71 492 km', '778 millions de km', '11 ans 315 j'},
+                'saturne' : {'5,68 X 10^26', '58 232 km', '1,4 milliards de km', '29 a et 167 j'},
+                'uranus' : {'8,6 X 10^25', '51 118 km','2,8 milliards de km', '84 ans'},
+                'neptune' : {'102 X 10^24', '24 764', '4,5 milliards de km', '165 ans'}}
+
+#fin des données
--- simulator/interface.py
+++ simulator/interface.py
@@ -1,7 +1,9 @@
+from multiprocessing.sharedctypes import Value
 import kepler as kp
 import pygame
 import sys
 from time import time
+import donnesplanetes as data
 
 BLACK = (0, 0, 0)
 GRAY = (20, 20, 20)
@@ -10,6 +12,16 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+
+#repère pour le dico des planetes (touche pas Paul)
+mercure = 1
+venus = 2 
+terre = 3
+mars = 4
+jupyter = 5
+saturne = 6
+uranus = 7
+neptune = 8
 
 width, height = 1080, 600 # dimensions de l'écran, en pixels 1080, 720
 pygame.init()
@@ -24,8 +36,34 @@ def update_time(temps, j, last_frame=time()):
     new_frame = time()
     return temps + (j/365.25)*(new_frame-last_frame), new_frame
 
+class ecran():
+
+    def espace_donnee(self):
+        #dessine une zone pour photo planete et infos en dessous 
+        pygame.draw.rect(screen, GRAY, 900, 0, 1080, 250)
+        pygame.draw.rect(screen, WHITE, 900, 250, 1080, 600)
+        pygame.display.flip()
+
+    def barre_action(self):
+        #creer une barre sur la gauche pour ajouter boutons et actions 
+        pygame.draw.rect(screen, GRAY, 0, 0, 50, 600)
+        pygame.draw.rect(screen, GRAY, 50, 0, 55, 600)
+        pygame.display.flip()
+
+    def affichage_info(self):
+        #pour retrouver les donnees associer a la planete dans le dico
+        #retourne sous forme de dico : {poids, rayon, distance au soleil, temps rotation soleil}
+        for i in data:
+            if i == data.key:
+                donneesaafficher = data.value
+        return donneesaafficher
+        
+
+
+
 def main():
-    
+    donnee = ecran.espace_donnee
+    barre = ecran.barre_action
     sunpos = (int(width/2), int(height/2))
 
     moon = kp.Planete(periapsis=100, apoapsis=450, center_of_mass=sunpos)
@@ -35,6 +73,8 @@ def main():
     frame_time = time()
     
     while True:
+        donnee()
+        barre()
         dt = clock.tick(144)
         screen.fill(BLACK)
         for event in pygame.event.get() :
